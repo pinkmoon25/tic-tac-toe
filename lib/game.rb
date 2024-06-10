931e297388646663aaf27ed6821f5734aabb5844
tushar <singhtushar348@gmail.com>
2024-06-10 17:21:51 +0530

    Colorize the game

--- lib/game.rb
+++ lib/game.rb
@@ -7,13 +7,7 @@ class Game
   end
 
   def board
-    puts "
-		#{@hash['1']} | #{@hash['2']} | #{@hash['3']}
-		----------
-		#{@hash['4']} | #{@hash['5']} | #{@hash['6']}
-		----------
-		#{@hash['7']} | #{@hash['8']} | #{@hash['9']}
-		"
+    puts " \n\n #{@hash['1']} | #{@hash['2']} | #{@hash['3']}\n ---------- \n #{@hash['4']} | #{@hash['5']} | #{@hash['6']} \n ---------- \n #{@hash['7']} | #{@hash['8']} | #{@hash['9']}\n\n"
   end
 
   def update_board(pos, symbol)
@@ -21,15 +15,15 @@ class Game
   end
 
   def end_game?
-		rows = [1, 4, 7].map { |i| [@hash[i.to_s], @hash[(i + 1).to_s], @hash[(i + 2).to_s]] }
+    rows = [1, 4, 7].map { |i| [@hash[i.to_s], @hash[(i + 1).to_s], @hash[(i + 2).to_s]] }
     columns = [1, 2, 3].map { |i| [@hash[i.to_s], @hash[(i + 3).to_s], @hash[(i + 6).to_s]] }
     diagonal = [[@hash['1'], @hash['5'], @hash['9']], [@hash['3'], @hash['5'], @hash['7']]]
 
     entry = rows + columns + diagonal
 
-    if entry.any? { |r| r.all? { |cell| cell.downcase == 'x' } }
+    if entry.any? { |r| r.all? { |cell| cell.uncolorize.downcase == 'x' } }
       'X WON!'
-    elsif entry.any? { |r| r.all? { |cell| cell.downcase == 'o' } }
+    elsif entry.any? { |r| r.all? { |cell| cell.uncolorize.downcase == 'o' } }
       'O WON!'
     elsif entry.flatten.uniq.length == 2
       "IT'S A TIE!"
--- main.rb
+++ main.rb
@@ -1,48 +1,52 @@
 # frozen_string_literal: true
 
+require 'colorize'
 require_relative 'lib/game'
 
 def main
   game = Game.new
-  puts " Let's Play! "
+  puts " Let's Play! ".bold.italic.light_yellow
   game.board
-  puts " Who goes first: 'X' or 'O'?"
 
-  current_symbol = pick_turn
-  play(current_symbol, game)
+  current_turn = pick_side
 
-  puts game.end_game?
+  play(current_turn, game)
+
+  puts game.end_game?.bold.light_green.blink_slow
 rescue StandardError
-  puts ' Alright See Ya Later!'
+  puts ' Alright See Ya Later!'.bold.light_white
 rescue Interrupt
-  puts ' Alright See Ya Later!'
+  puts ' Alright See Ya Later!'.bold.light_white
 end
 
-def pick_turn
-  current_symbol = nil
+def pick_side
+  puts " #{'Who goes first:'.bold.light_blue} #{'X'.bold.red} or #{'O'.bold.green}?"
+  current_turn = nil
   loop do
     user_input = gets.chomp.upcase
     if %w[X O].include?(user_input)
-      current_symbol = user_input
+
+      current_turn = user_input
       break
     else
-      puts " Invalid input! Please choose 'X' or 'O'."
+      puts " Invalid input! Please choose 'X' or 'O'.".light_red.italic
     end
   end
-  current_symbol
+  current_turn.downcase == 'x' ? current_turn.bold.red : current_turn.bold.green
 end
 
 def play(symbol, game)
   loop do
-    puts " #{symbol}'s turn: Pick a spot"
+    puts " #{symbol}" + "'s turn : Pick a spot".bold.gray
     value = gets.chomp.to_i
+
     if value.between?(1, 9) && game.update_board(value.to_s, symbol)
       game.board
       break if game.end_game?
 
-      symbol = symbol == 'X' ? 'O' : 'X'
+      symbol = symbol.uncolorize.upcase == 'X' ? 'O'.bold.green : 'X'.bold.red
     else
-      puts ' Invalid move! Please pick an empty spot (1-9).'
+      puts ' Invalid move! Please pick an empty spot (1-9).'.light_red.italic
     end
   end
 end
